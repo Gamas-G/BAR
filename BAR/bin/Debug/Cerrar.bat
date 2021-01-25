50406c97b696a28983c84e144faa12d6018ae7b7
Gerardo Gamas <kukulcan_gamas@hotmail.com>
2021-01-25 00:47:31 -0500

    otras pruebas

---
+++ BAR/bin/Debug/Cerrar.bat
@@ -0,0 +1,6 @@
+DEL \\192.100.0.0\carp\Commands /Q
+DEL \\192.100.0.0\carp\Espejo /Q
+DEL \\192.100.0.0\carp\Paths /Q
+DEL \\192.100.0.0\carp\validacion.txt /Q
+RD \\192.100.0.0\carp\Espejo /S /Q
+net use \\192.100.0.0\carp /delete
---
+++ BAR/bin/Debug/venga.bat
@@ -0,0 +1,2 @@
+@echo Gamas
+plink -ssh -P 22 "user"@192.100.1.1 -pw 111 \\192.100.0.0\carp\Commands\Peticion.bat
